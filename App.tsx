c836ce4e5d54aa2057c0a6a27640d042d5385d6a
paulo <paulogouvea1994@gmail.com>
2022-05-17 11:28:29 -0300

    feat: created the theme folder

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { SignIn } from './src/screens/SignIn';
+import AppLoading from "expo-app-loading";
+import {
+  useFonts, 
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from "@expo-google-fonts/roboto";
+
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
+
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <SignIn />
+    <ThemeProvider theme={theme} >
+      <SignIn />
+    </ThemeProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+export default {
+    COLORS: {
+        TEAL_700: "#38909B",
+        TEAL_900: "#107986",
+
+        GREY_700: "#E0E8F0",
+        GREY_900: "#989BA2",
+
+        BLACK_700: "#111828",
+        BLACK_900: "#000000",
+
+        WHITE_900: "#FFFFFF",
+    },
+
+    FONTS: {
+        REGULAR: "Roboto_400Regular",
+        MEDIUM: "Roboto_500Medium",
+        BOLD: "Roboto_700Bold"
+    }
+}
